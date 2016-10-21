974cb919cb4b311209258f3603dd9b9e6650fc36
James Cancilla <cancilla@cancilla.local>
2016-10-21 07:25:09 -0400

    adding code

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dsxutils',
+  packages = ['dsxutils'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Utils for working in DSX',
+  author = 'James Cancilla',
+  author_email = 'cancilla.streams@gmail.com',
+  url = 'https://github.com/cancilla/dsx', # use the URL to the github repo
+  download_url = 'https://github.com/cancilla/dsx/tarball/0.1', # I'll explain this in a second
+  keywords = ['dsx', 'utils'], # arbitrary keywords
+  classifiers = [],
+)
