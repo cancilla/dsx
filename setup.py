893dfa2de3823b0b391d50f2d42f8488e29b79c5
James Cancilla <cancilla@ca.ibm.com>
2016-10-21 08:08:57 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'dsxutils',
   packages = ['dsxutils'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.1',
   description = 'Utils for working in DSX',
   author = 'James Cancilla',
   author_email = 'cancilla.streams@gmail.com',
